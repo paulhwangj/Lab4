e4698dba39aeef3bfb175af531a124c2255fc597
paulhwangj <pauljunhwang@gmail.com>
2022-10-19 20:29:07 -0500

    added lab description and reflection

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,11 +2,16 @@
 {
     /**
         Name: Paul Hwang
-        Date: October 18th, 2022
-        Description: Lab 3 - Building upon our Lab 2 and having the database be an actual running database
-					         on bit.io. I use an API key to write and fetch data to/from the database.
-        Bugs: 
-        Reflection: 
+        Date: October 19th, 2022
+        Description: Lab 4 - Using our previous lab submission and creating unit tests for it to ensure
+                             that our functions work properly (both happy and unhappy paths)
+        Bugs: N/A
+        Reflection: Setting up the testing environment was a disaster on Mac. I tried to run parallels but it kept giving me
+                    errors so I ended up just doing the lab on my desktop. After I switched over to my desktop, the process was
+                    pretty linear and it ended up working just fine. The only issue I have is that we're testing against the actual
+                    database rather than mocking it out. I originally had a "TestDatabase" class that mocked out the functionality of the
+                    RelationalDatabase but was informed to just test against the actual bit.io database. It resulted in me having to
+                    rework my code a bit but all my tests have passed and seem to be working.
     */
 
     public static class MauiProgram
